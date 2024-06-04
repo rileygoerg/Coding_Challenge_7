ab2f03e6147cfe0b2bf68b72de0071640245e86f
rileygoerg <rgoerg@usf.edu>
2024-06-04 17:47:44 -0400

    Finalize feedback display functionality.

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
+// U14123683
 document.getElementById("feedbackForm").addEventListener("submit",(event)=>{
-    event.preventDefault();
+    event.preventDefault(); // Prevents auto reload
 
-    const first = document.getElementById("firstName").value;
-    const last = document.getElementById("lastName").value;
-    const rating = document.getElementById("ratingInput").value;
-    const notes = document.getElementById("feedbackInput").value;
+    const first = document.getElementById("firstName").value; // taking input value for first name
+    const last = document.getElementById("lastName").value;   // last name 
+    const rating = document.getElementById("ratingInput").value; // rating 
+    const notes = document.getElementById("feedbackInput").value; // feedback
 
-    if (first.trim == "" || last.trim == "" || rating == "" || notes == "") {
-        alert("Please fill out every field.")
-    } else {
-        alert("Feedback submitted.")
-        document.getElementById("displayFeedback").innerHTML = `
-        <h2>Feedback Received</h2>
+    if (first.trim == "" || last.trim == "" || rating == "" || notes == "") { // if any inputs are blank,
+        alert("Please fill out every field.") // an alert will pop up
+    } else { // if all inputs are filled out,
+        alert("Feedback submitted.") 
+        document.getElementById("displayFeedback").innerHTML = // all inputs will be displayed in the div tag
+        `<h2>Feedback Received</h2>
         <p><strong>Name: </strong>${first} ${last}</p>
-        <p><strong>Rating: </strong> ${rating}</p>
+        <p><strong>Rating: </strong> ${rating}/5</p>
         <p><strong>Comments: </strong> ${notes}</p>`
     };
 })
